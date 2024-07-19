64d345093ede4a19eef751a324a56003d75f6664
Timothy Karani <kartimothy@gmail.com>
2024-07-20 02:57:36 +0300

    Added SHADCN/UI components

---
+++ src/components/shared/admin-layout.tsx
@@ -0,0 +1,182 @@
+"use client";
+import React, { Fragment } from "react";
+import Link from "next/link";
+import {
+  Home,
+  LineChart,
+  Package,
+  Package2,
+  PanelLeft,
+  Search,
+  ShoppingCart,
+  User,
+  Users2,
+} from "lucide-react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useUserSession } from "../../../hooks/use-user-session";
+import { signOutWithGoogle } from "@/lib/firebase/auth";
+import { removeSession } from "@/actions/auth-actions";
+
+interface Crumbs {
+  href: string;
+  label: string;
+}
+
+export default function AdminLayout({
+  children,
+  breadcrumbs,
+  session,
+  className,
+  query = "",
+  setQuery = () => {},
+}: {
+  children: React.ReactNode;
+  breadcrumbs: Crumbs[];
+  session: string | null;
+  className?: string;
+  query?: string;
+  setQuery?: (query: string) => void;
+}) {
+  const userSessionId = useUserSession(session);
+
+  const handleSignOut = async () => {
+    await signOutWithGoogle();
+    await removeSession();
+  };
+
+  return (
+    <>
+      <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="sm:hidden">
+              <PanelLeft className="h-5 w-5" />
+              <span className="sr-only">Toggle Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="sm:max-w-xs">
+            <nav className="grid gap-6 text-lg font-medium">
+              <Link
+                href="#"
+                className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+              >
+                <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+                <span className="sr-only">Acme Inc</span>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <Home className="h-5 w-5" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <ShoppingCart className="h-5 w-5" />
+                Orders
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-foreground"
+              >
+                <Package className="h-5 w-5" />
+                Products
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <Users2 className="h-5 w-5" />
+                Customers
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <LineChart className="h-5 w-5" />
+                Settings
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <Breadcrumb className="hidden md:flex">
+          <BreadcrumbList>
+            {breadcrumbs.slice(0, -1).map((crumb, index) => (
+              <Fragment key={index}>
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link href={crumb.href}>{crumb.label}</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+              </Fragment>
+            ))}
+
+            <BreadcrumbItem>
+              <BreadcrumbPage>{breadcrumbs.at(-1)?.label}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="relative ml-auto flex-1 md:grow-0">
+          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            placeholder="Search..."
+            className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="overflow-hidden rounded-full"
+            >
+              <User className="h-5 w-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            {userSessionId && (
+              <DropdownMenuItem onClick={handleSignOut}>
+                Logout
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+
+      <main
+        className={`grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 ${className}`}
+      >
+        {children}
+      </main>
+    </>
+  );
+}
